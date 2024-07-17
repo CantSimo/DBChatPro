8e76ff2157b265fe1122c88c3d983b8ccffd91a4
BLUENEXT\simone.cantelmi <simone.cantelmi@bluenext.it>
2024-07-17 10:06:54 +0200

    response formatted + experimentation with agents

--- AIService/handlers/base.py
+++ AIService/handlers/base.py
@@ -7,13 +7,17 @@ from operator import itemgetter
 from utils.alerts import alert_exception, alert_info
 from typing import List
 from langgraph.prebuilt import create_react_agent
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents import (create_openai_tools_agent, agent_types)
+from langchain.agents.agent import AgentExecutor
+from langchain_community.callbacks.manager import get_openai_callback
+from langchain_community.agent_toolkits import SQLDatabaseToolkit, create_sql_agent
+from langchain_community.agent_toolkits.sql.prompt import SQL_FUNCTIONS_SUFFIX
 from langchain_community.utilities import SQLDatabase
-from langchain_community.agent_toolkits import create_sql_agent
-from langchain_core.prompts import (
+from langchain_core.prompts.chat import (
     ChatPromptTemplate, 
     MessagesPlaceholder,
-    PromptTemplate
+    PromptTemplate,
+    HumanMessagePromptTemplate
 )
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -57,10 +61,10 @@ class BaseHandler():
 
         self.chat_model = chat_model
                
-    def chat(self, connection_string: str, query: str, chat_history: list[str] = []):
+    def chat(self, connection_string: str, user_question: str, chat_history: list[str] = []):
         """
         connection_string: str
-        query: str
+        user_question: str
         chat_history: list of previous chat messages
         kwargs:
             namespace: str
@@ -82,7 +86,7 @@ class BaseHandler():
             llm=ChatOpenAI(model='gpt-3.5-turbo', temperature=0.2, openai_api_key=os.getenv('OPENAI_API_KEY'))
 
             # ************************************************************
-            # SQL Query chain 
+            # create_sql_query_chain - [OK]
             # ************************************************************
             system  = """You are a {dialect} expert. Given an input question, create a syntactically correct {dialect} query to run.
                     Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database.
@@ -120,61 +124,160 @@ class BaseHandler():
                 if "Final answer: " in output:
                         return output.split("Final answer: ")[1]
                 else:
-                    # Gestione del caso in cui "Final answer" non è presente
-                    print("No 'Final answer' found in the output.")             
-                    print("**********************************************")
-                    return output
+                    if "First draft: " in output:
+                            return output.split("First draft: ")[1]
+                    else:
+                        # Gestione del caso in cui "Final answer" non è presente
+                        print("No 'Final answer' found in the output.")             
+                        print("**********************************************")
+                        return output
 
             chain = create_sql_query_chain(llm, db, prompt=prompt) | parse_final_answer
 
-            response = chain.invoke(
-                        {
-                            "question": query
-                        }
-                    )
+            with get_openai_callback() as cb:
+                response = chain.invoke(
+                            {
+                                "question": user_question
+                            }
+                        )
+
+            print("**********************************************")
+            print(cb)
+
+            # Extract relevant information from cb
+            openai_fee = {
+                "completion_tokens": cb.completion_tokens,  
+                "prompt_tokens": cb.prompt_tokens ,
+                "total_cost": cb.total_cost
+            }
             
-            return response
-        
+            return {
+                "response": response,
+                "openai_fee": openai_fee
+            }
+
             # ************************************************************
-            # Agents 
+            # create_sql_agent - [failed]
+            # ************************************************************           
+            # toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+            # context = toolkit.get_context() 
+            # tools = toolkit.get_tools()
+            # agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True, tools=tools, context=context)
+            # with get_openai_callback() as cb:
+            #     response=agent_executor.invoke({user_question})
+
+            # print("**********************************************")
+            # print(cb)
+
+            # # Extract relevant information from cb
+            # openai_fee = {
+            #     "completion_tokens": cb.completion_tokens,  
+            #     "prompt_tokens": cb.prompt_tokens ,
+            #     "total_cost": cb.total_cost
+            # }
+            
+            # return {
+            #     "response": response,
+            #     "openai_fee": openai_fee
+            # }
+
             # ************************************************************
+            # create_openai_tools_agent - [failed]
+            # ************************************************************           
+            # toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+            # context = toolkit.get_context()
+            # tools = toolkit.get_tools()
 
-            toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-            tools = toolkit.get_tools()
-            tools
+            # messages = [
+            #     HumanMessagePromptTemplate.from_template("{input}"),
+            #     AIMessage(content=SQL_FUNCTIONS_SUFFIX),
+            #     MessagesPlaceholder(variable_name="agent_scratchpad"),
+            # ]
 
-            SQL_PREFIX = """You are an agent designed to interact with a SQL Server database.
-            Given an input question, create a syntactically correct SQL Server query to run, then look at the results of the query and return the answer.
-            Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
-            You can order the results by a relevant column to return the most interesting examples in the database.
-            Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-            You have access to tools for interacting with the database.
-            Only use the below tools. Only use the information returned by the below tools to construct your final answer.
-            You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+            # prompt = ChatPromptTemplate.from_messages(messages)
+            # prompt = prompt.partial(**context)            
+            # agent = create_openai_tools_agent(llm, tools, prompt)
 
-            DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+            # agent_executor = AgentExecutor(
+            #     agent=agent,
+            #     tools=toolkit.get_tools(),
+            #     verbose=True,
+            # )
 
-            To start you should ALWAYS look at the tables in the database to see what you can query.
-            Do NOT skip this step.
-            Then you should query the schema of the most relevant tables."""
+            # with get_openai_callback() as cb:
+            #     response=agent_executor.invoke({user_question})
 
-            system_message = SystemMessage(content=SQL_PREFIX)
+            # print("**********************************************")
+            # print(cb)
 
-            agent_executor = create_react_agent(llm, tools, messages_modifier=system_message)
+            # # Extract relevant information from cb
+            # openai_fee = {
+            #     "completion_tokens": cb.completion_tokens,  
+            #     "prompt_tokens": cb.prompt_tokens ,
+            #     "total_cost": cb.total_cost
+            # }
+            
+            # return {
+            #     "response": response,
+            #     "openai_fee": openai_fee
+            # }
 
-            alert_info(f"Agent working on task..")
+            # ************************************************************
+            # create_react_agent - [OK]
+            # ************************************************************
+            # toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+            # tools = toolkit.get_tools()
+            # tools
 
-            response = agent_executor.invoke({"messages": [HumanMessage(content=f"{query}")]})
+            # SQL_PREFIX = """You are an agent designed to interact with a SQL Server database.
+            # Given an input question, create a syntactically correct SQL Server query to run, then look at the results of the query and return the answer.
+            # You can order the results by a relevant column to return the most interesting examples in the database.
+            # Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+            # You have access to tools for interacting with the database.
+            # Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+            # You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 
-            alert_info(f"Task succesfully")
+            # DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-            # for s in agent_executor.stream(
-            #     {"messages": [HumanMessage(content=f"{query}")]}
-            # ):
-            #     print(s)
-            #     print("----")
+            # To start you should ALWAYS look at the tables in the database to see what you can query.
+            # Do NOT skip this step.
+            # Then you should query the schema of the most relevant tables.
+            # ALWAYS return the final query you used to give the response.
+            # Use ALWAYS format:
 
-            return response
+            # Query: <<FINAL_QUERY>>
+            # Final answer: <<FINAL_ANSWER>>
+            # """
+            # system_message = SystemMessage(content=SQL_PREFIX)
+
+            # agent_executor = create_react_agent(llm, tools, messages_modifier=system_message)
+
+            # with get_openai_callback() as cb:
+            #     response = agent_executor.invoke({"messages": [HumanMessage(content=f"{user_question}")]})
+
+            # print("**********************************************")
+            # print(cb)
+
+            # # for s in agent_executor.stream(
+            # #     {"messages": [HumanMessage(content=f"{user_question}")]}
+            # # ):
+            # #     print(s)
+            # #     print("----")
+
+            # # Extract relevant information from cb
+            # openai_fee = {
+            #     "completion_tokens": cb.completion_tokens,  
+            #     "prompt_tokens": cb.prompt_tokens ,
+            #     "total_cost": cb.total_cost
+            # }
+            
+            # last_message = response['messages'][-1]
+
+            # return {
+            #     "response": last_message.content,
+            #     "openai_fee": openai_fee
+            # }
+
         except Exception as e:
             alert_exception(e, "Error chatting")
             raise HTTPException(status_code=500, detail=f"Error chatting: {str(e)}")
--- DBChatPro/Models/AIConnection.cs
+++ DBChatPro/Models/AIConnection.cs
@@ -1,4 +1,6 @@
-﻿namespace DBChatPro
+﻿using System.Text.Json.Serialization;
+
+namespace DBChatPro
 {
     public class AIConnection
     {
@@ -15,10 +17,35 @@
         public double Temperature { get; set; }
         public List<string> Chat_History { get; set; } = new List<string>();
     }
+    public class OpenAIApiResponse
+    {
+        [JsonPropertyName("response")]
+        public ResponseData Response { get; set; }
+    }
 
     public class ResponseData
     {
-        public string Response { get; set; }
+        [JsonPropertyName("response")]
+        public string Query { get; set; }
+
+        [JsonPropertyName("openai_fee")]
+        public OpenAIFee OpenAIFee { get; set; }
     }
 
+    public class OpenAIFee
+    {
+        [JsonPropertyName("completion_tokens")]
+        public int CompletionTokens { get; set; }
+
+        [JsonPropertyName("prompt_tokens")]
+        public int PromptTokens { get; set; }
+
+        [JsonPropertyName("total_cost")]
+        public double TotalCost { get; set; }
+
+        public override string ToString()
+        {
+            return $"completion_tokens:[{CompletionTokens.ToString()}]\nprompt_tokens:[{PromptTokens.ToString()}]\ntotal_cost:[{TotalCost.ToString()}$]";
+        }
+    }
 }
