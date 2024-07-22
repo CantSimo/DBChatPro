3046da3a1dc5ab5d30972d57b075101851768789
BLUENEXT\simone.cantelmi <simone.cantelmi@bluenext.it>
2024-07-22 15:47:23 +0200

    +gpt-4o-mini [wip]

--- AIService/handlers/base.py
+++ AIService/handlers/base.py
@@ -1,4 +1,5 @@
 import os
+import re
 import tiktoken
 import warnings
 warnings.filterwarnings("ignore")
@@ -20,6 +21,7 @@ from langchain_core.prompts.chat import (
     HumanMessagePromptTemplate
 )
 from langchain_core.output_parsers import StrOutputParser
+from langchain.output_parsers.structured import  StructuredOutputParser, ResponseSchema
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import (
     HumanMessage, 
@@ -84,8 +86,13 @@ class BaseHandler():
             # Get GPT model
             # llm=self.llm_map[self.chat_model] 
             llm=ChatOpenAI(model='gpt-3.5-turbo', temperature=0.2, openai_api_key=os.getenv('OPENAI_API_KEY'), verbose=True)
-            # llm=ChatOpenAI(model='gpt-4o-mini', temperature=0.2, openai_api_key=os.getenv('OPENAI_API_KEY'), verbose=True)
+            # llm=ChatOpenAI(
+            #      model='gpt-4o-mini', 
+            #      temperature=0.2, 
+            #      openai_api_key=os.getenv('OPENAI_API_KEY'), 
+            #      verbose=True)
 
+            # Prompt for gpt-3.5-turbo    
             # ************************************************************
             # create_sql_query_chain - [OK]
             # ************************************************************
@@ -107,17 +114,50 @@ class BaseHandler():
                     - Using the correct number of arguments for functions
                     - Casting to the correct data type
                     - Using the proper columns for joins
-
+                    
+                    Question: {input}
+                    
                     Use ALWAYS format:
 
                     First draft: <<FIRST_DRAFT_QUERY>>
                     Final answer: <<FINAL_ANSWER_QUERY>>
                     """
 
+            # Prompt for gpt-4o-mini
+            # system  = '''You are a {dialect} expert. Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+            #         Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the TOP clause as per {dialect}. You can order the results to return the most informative data in the database.
+            #         Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+            #         Pay attention to use only the column names you can see in the tables below. BE CAREFUL to not query for columns that do not exist. Also, pay attention to which column is in which table.
+            #         Pay attention to use date('now') function to get the current date, if the question involves "today".
+
+            #         Only use the following tables:
+            #         {table_info}
+
+            #         Write an initial draft of the query. Then double check the {dialect} query for common mistakes, including:
+            #         - Using NOT IN with NULL values
+            #         - Using UNION when UNION ALL should have been used
+            #         - Using BETWEEN for exclusive ranges
+            #         - Data type mismatch in predicates
+            #         - Properly quoting identifiers
+            #         - Using the correct number of arguments for functions
+            #         - Casting to the correct data type
+            #         - Using the proper columns for joins
+            #         - double check columns name with database schema
+                    
+            #         Use the following format:
+
+            #         Question: "Question here"
+            #         SQLQuery: "SQL Query to run"
+            #         SQLResult: "Result of the SQLQuery"
+            #         Answer: "Final answer here"
+
+            #         Question: {input}'''
+
             prompt = ChatPromptTemplate.from_messages(
-                [("system", system), ("human", "{input}")]
+                [("system", system)]
             ).partial(dialect=db.dialect)
 
+            # Parser for gpt-3.5-turbo
             def parse_final_answer(output: str) -> str:
                 print("**********************************************")
                 print(output)
@@ -132,8 +172,23 @@ class BaseHandler():
                         print("No 'Final answer' found in the output.")             
                         print("**********************************************")
                         return output
-
+                    
+            # Parser for gpt-4o-mini
+            def extract_sql_query(input_string: str) -> str:
+                # Regex pattern to match the SQL query within triple backticks
+                pattern = r'SQLQuery:\s*```sql\n(.*?)\n```'
+                
+                # Search for the pattern in the input string
+                match = re.search(pattern, input_string, re.DOTALL)
+                
+                if match:
+                    # Return the cleaned SQL query
+                    return match.group(1).strip()
+                else:
+                    return None
+    
             chain = create_sql_query_chain(llm, db, prompt=prompt) | parse_final_answer
+            # chain = create_sql_query_chain(llm, db, prompt=prompt) | extract_sql_query
             
             #chain.get_prompts()[0].pretty_print()
             #print("**********************************************")
